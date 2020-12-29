5dd9afb687c18998c2b6c5f3cbb7ffd389dcde55
Tien Vu <69743095+dumfoundead@users.noreply.github.com>
2020-12-28 21:01:40 -0500

    Add files via upload

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_notes (note_name, note_content, folder)
+VALUES
+  ('First note', 'Here is some note content wow what a great note', '1'),
+  ('Second note', 'This note has some pretty good content too but not as good as first note', '1'),
+  ('Third note', 'this is some content for a third note i guess', '2'),
+  ('Fourth note', 'i think these could even be a short story collection if you really think about it', '3'),
+  ('Fifth note', 'this is the last note i will be typing this evening so enjoy this content', '4');
